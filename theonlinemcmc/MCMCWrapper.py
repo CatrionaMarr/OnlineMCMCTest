92899d082c1bdfc2d61128ced453ac59812ae03a
Cat <catrionamarr95@gmail.com>
2018-03-09 20:15:44 +0000

    same as before

--- theonlinemcmc/MCMCWrapper.py
+++ theonlinemcmc/MCMCWrapper.py
@@ -15,94 +15,99 @@ from theonlinemcmc import *
 #errval = 0
 
 class MCMCWrapper(object): # fix arg names before running idiot
-	def __init__(self, sampler, theta, gauss_like_sigma, fitarray, Nmcmc, Nburnin, Nens, ndim, errval, emailaddress, outdir, modelStrings, argslist):
+	def __init__(self, sampler, theta, abscissastring, three, Nmcmc, Nburnin, Nens, ndim, emailaddress, outdir, modelStrings, argslist):
 		self.sampler = sampler
-    self.theta = theta
-		self.gauss_like_sigma = gauss_like_sigma
-		self.fitarray = fitarray
+		self.theta = theta
 		self.Nmcmc = Nmcmc
-    self.Nburnin = Nburnin
-    self.Nens = Nens
-    self.ndim = ndim
-    self.errval = errval
-    self.emailaddress = emailaddress
-    self.outdir = outdir
-    self.modelStrings = modelStrings
-    self.argslist = argslist
+		self.Nburnin = Nburnin
+		self.Nens = Nens
+		self.ndim = ndim
+		self.emailaddress = emailaddress
+		self.outdir = outdir
+		self.modelStrings = modelStrings
+		self.argslist = argslist
+		self.errval = 0
+		#self.run_mcmc()
+		#self.run_postproc()
 
-		self.run_mcmc()
-		self.run_postproc()
+		self.resultsaddress = "http://localhost/results/" + self.outdir
+		#thetajoinquotes = "'%s'"%thetajoin
 
-  for name in theta:
-    thetajoin = self.thetajoin + ", " + name
-  
-	resultsaddress = "http://localhost/results/" + self.outdir
-	thetajoinquotes = "'%s'"%thetajoin
-
-	emailaddressquotes = "'%s'"%self.emailaddress
-	resultsaddressquotes = "'%s'"%self.resultsaddress
-	outdirquotes = "'%s'"%self.outdir
-	modelStringsquotes = "'%s'"%self.modelStrings['outputstring']
-	
-	# read in the data
-	if self.errval == 0:
-  		try:
-    		data = np.loadtxt("data_file.txt")
-  		except:
-    		try:
-      			data = np.loadtxt("data_file.txt", delimiter=",")
-    		except:
-      			self.errval = DATA_READ_ERR
-
-
-	# read in the abscissa values
-	if self.errval == 0:
-  		try:
-    		abscissastring = np.loadtxt("abscissa_file.txt")
-  		except:
-    		try:
-      			abscissastring = np.loadtxt("abscissa_file.txt", delimiter=",")
-    		except:
-      			self.errval = ABSCISSA_READ_ERR
-
-  abscissastringquotes = "'%s'"abscissastring
+		#emailaddressquotes = "'%s'"%self.emailaddress
+		#resultsaddressquotes = "'%s'"%self.resultsaddress
+		#outdirquotes = "'%s'"%self.outdir
+		#modelStringsquotes = "'%s'"%self.modelStrings['outputstring']
+		logfile = open("../logfile.txt","w")
+		logfile.write("print 1")
+		logfile.close()
+		# read in the data
+		if self.errval == 0:
+  			try:
+				data = np.loadtxt("data_file.txt")
+				logfile = open("../logfile.txt","a")
+				logfile.write("print 2")
+				logfile.close()
+			except:
+				try:
+					data = np.loadtxt("data_file.txt", delimiter=",")
+				except:
+					self.errval = DATA_READ_ERR
 
 
-	def run_mcmc():
-		if self.sampler == "emcee":
+		# read in the abscissa values
+		if self.errval == 0:
+			try:
+				abscissastring = np.loadtxt("abscissa_file.txt")
+				logfile = open("../logfile.txt","a")
+				logfile.write("print 3")
+				logfile.close()
+			except:
+				try:
+					abscissastring = np.loadtxt("abscissa_file.txt", delimiter=",")
+				except:
+					self.errval = ABSCISSA_READ_ERR
+
+		abscissastringquotes = "'%s'"%abscissastring
+
+
+	def run_mcmc(self):
+		if self.sampler == 'emcee':
+			logfile = open("../logfile.txt","a")
+			logfile.write("print 4")
+			logfile.close()
 			# define the log posterior function
 
 			def lnprob(theta, abscissastring, gauss_like_sigma, data):
-  				lp = lnprior(self.theta)
-  				if not np.isfinite(lp):
-    				return -np.inf
+				lp = lnprior(self.theta)
+				if not np.isfinite(lp):
+					return -np.inf
 
-  				return lp + lnlike(theta, abscissastring, gauss_like_sigma, data)
+				return lp + lnlike(theta, abscissastring, gauss_like_sigma, data)
 			
 
 			# define the log prior function -------FITARRAY
 			def lnprior(theta):
-  				lp = 0.
+				lp = 0.
   				
-  				for variable in self.theta:
-  			   		if self.fitarray[variable][minval] < self.theta[variable] < self.fitarray[variable][maxval]:
+				for variable in self.theta:
+					if self.fitarray[variable][minval] < self.theta[variable] < self.fitarray[variable][maxval]:
   					#if -100 < m < 100:
-    					lp = 0.
-  					else:
-    					return -np.inf
-    				"""	
+						lp = 0.
+					else:
+						return -np.inf
+    					"""	
   					if -101 < c < 101:
     					lp = 0.
   					else:
     					return -np.inf
 					"""
-  					return lp
+					return lp
 
 			# define log likelihood function
 			def lnlike(theta, abscissastring, gauss_like_sigma, data):
   				#m,c = theta
-  				md = mymodel(self.modelStrings[arguments])
-  				return -0.5*np.sum(((md - self.data)/self.gauss_like_sigma)**2)
+				md = mymodel(self.modelStrings[arguments])
+				return -0.5*np.sum(((md - self.data)/self.gauss_like_sigma)**2)
   				"""
 				# set number of MCMC points
 				Nmcmc = 1000
@@ -116,65 +121,65 @@ class MCMCWrapper(object): # fix arg names before running idiot
 					varini[variable] = self.fitarray[variable][minval] + np.random.rand(self.Nens) * (self.fitarray[variable][maxval] - self.fitarray[variable][minval])
 					#mini = -100 + np.random.rand(Nens)*200
   					#cini = -101 + np.random.rand(Nens)*202
-  					pos = np.array(varini).T
+					pos = np.array(varini).T
 				except:
-  					self.errval = PRIOR_INIT_ERR
+					self.errval = PRIOR_INIT_ERR
 
 			# read in the data
 			if self.errval == 0:
-  				try:
-    				data = np.loadtxt("data_file.txt")
-  				except:
-    				try:
-      					data = np.loadtxt("data_file.txt", delimiter=",")
-    				except:
-      					self.errval = DATA_READ_ERR
+				try:
+					data = np.loadtxt("data_file.txt")
+				except:
+					try:
+						data = np.loadtxt("data_file.txt", delimiter=",")
+    					except:
+      						self.errval = DATA_READ_ERR
 
 			# read in the abscissa values
 			if self.errval == 0:
-  				try:
-    				abscissastring = np.loadtxt("abscissa_file.txt")
-  				except:
-    				try:
-      					abscissastring = np.loadtxt("abscissa_file.txt", delimiter=",")
-    				except:
-      					self.errval = ABSCISSA_READ_ERR
+				try:
+    					abscissastring = np.loadtxt("abscissa_file.txt")
+				except:
+    					try:
+						abscissastring = np.loadtxt("abscissa_file.txt", delimiter=",")
+					except:
+						self.errval = ABSCISSA_READ_ERR
 
 			# read in sigma (standard deviation) values (there may be nothing here if it not applicable to your likelihood)
 			#{readsigma}
 
 			# run the MCMC
 			if self.errval == 0:
-  				if len(data) != len(abscissastring):
-    				self.errval = DATA_LENGTH_ERR
+				if len(data) != len(abscissastring):
+					self.errval = DATA_LENGTH_ERR
 
   				#argslist = (abscissastring, 0.65, data)
 
 			if self.errval == 0:
   				# set up sampler
-  				try:
-    				finalsampler = emcee.EnsembleSampler(Nens, ndim, lnprob, args=argslist)
-  				except:
-    				self.errval = MCMC_INIT_ERR
+				try:
+					finalsampler = emcee.EnsembleSampler(Nens, ndim, lnprob, args=argslist)
+				except:
+					self.errval = MCMC_INIT_ERR
 
   				# run sampler
-  				try:
-    				finalsampler.run_mcmc(pos, Nmcmc+Nburnin)
+				try:
+					finalsampler.run_mcmc(pos, Nmcmc+Nburnin)
     				# remove burn-in and flatten
-    				samples = finalsampler.chain[:, self.Nburnin:, :].reshape((-1, self.ndim))
-    				lnp = np.reshape(finalsampler.lnprobability[:, self.Nburnin:].flatten(), (-1,1))
-    				samples = np.hstack((samples, lnp))
- 	 			except:
-    				self.errval = MCMC_RUN_ERR
+					samples = finalsampler.chain[:, self.Nburnin:, :].reshape((-1, self.ndim))
+					lnp = np.reshape(finalsampler.lnprobability[:, self.Nburnin:].flatten(), (-1,1))
+					samples = np.hstack((samples, lnp))
+	 			except:
+					self.errval = MCMC_RUN_ERR
 
   				# output the posterior samples, likelihood and variables
-  				try:
-    				np.savetxt('posterior_samples.txt.gz', samples)
-    				fv = open('variables.txt', 'w')
-    				fv.write(thetajoinquotes)
-    				fv.close()
-  				except:
-    				self.errval = POST_OUTPUT_ERR
+				try:
+					np.savetxt('posterior_samples.txt.gz', samples)
+					fv = open('variables.txt', 'w')
+					fv.write(thetajoinquotes)
+					fv.close()
+				except:
+					self.errval = POST_OUTPUT_ERR
 		
 
 
@@ -184,20 +189,20 @@ class MCMCWrapper(object): # fix arg names before running idiot
 		#elif self.sampler == "cpnest":
 			#run postproc in cpnest form
 
-	def run_postproc():
-
-		try:
-    		postprocessing(samples, thetajoinquotes, abscissastring, abscissastringquotes, data, emailaddressquotes, resultsaddressquotes)
-  		except:
-    		self.errval = POST_PROCESS_ERR
+	def run_postproc(self):
+		if self.errval == 0:
+			try:
+				postprocessing(samples, theta, abscissastring, abscissastring, data, emailaddress, resultsaddress)
+			except:
+				self.errval = POST_PROCESS_ERR
 
 		success = True
 		if self.errval != 0:
   			# run different script in case error codes are encountered
-  			errorpage(self.errval, emailaddressquotes, resultsaddressquotes)
-  			success = False
+			errorpage(self.errval, self.emailaddress, self.resultsaddress)
+			success = False
 
-		database_add_row(outdirquotes, self.modelStrings, thetajoinquotes, len(theta), success)
+		database_add_row(self.outdir, self.modelStrings, self.theta, len(self.theta), success)
 
 
 
